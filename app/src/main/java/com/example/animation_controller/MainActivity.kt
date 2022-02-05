86077a67f9bb2ea28ac4f1174e375e49f0b4f6ab
ParkJong-Hun <thesn515@gmail.com>
2022-02-05 13:28:38 +0900

    2022/02/05 13:28 init

---
+++ app/src/main/java/com/example/animation_controller/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.animation_controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/*애니메이션
+ */
